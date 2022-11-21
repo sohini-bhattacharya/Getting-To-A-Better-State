a31e26fb23989721bb842cac9f27d0655823ebca
ilaananta <69838058+ilaananta@users.noreply.github.com>
2022-11-21 19:21:12 +0530

    premium

---
+++ lib/user.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+class User extends StatefulWidget {
+  const User({Key? key}) : super(key: key);
+
+  @override
+  _UserState createState() => _UserState();
+}
+
+class _UserState extends State<User> {
+  @override
+  var premium='';
+  TextEditingController premium_Controller = TextEditingController();
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Container(),
+            Container(
+              padding: EdgeInsets.only(left: 50, top: 160),
+              child: Text(
+                'Predict your ',
+                style: TextStyle(color: Colors.white, fontSize: 33),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 50, top: 220),
+              child: Text(
+                'Premium ',
+                style: TextStyle(color: Colors.white, fontSize: 33),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(left: 35, right: 35),
+                      child: Column(
+                        children: [
+                          TextField(
+                            style: TextStyle(color: Colors.black),
+                            decoration: InputDecoration(
+                                fillColor: Colors.grey.shade100,
+                                filled: true,
+                                hintText: "Age",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          TextField(
+                            style: TextStyle(),
+
+                            decoration: InputDecoration(
+                                fillColor: Colors.grey.shade100,
+                                filled: true,
+                                hintText: "Gender",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          TextField(
+                            style: TextStyle(),
+
+                            controller: premium_Controller..text=premium,
+                            decoration: InputDecoration(
+                                fillColor: Colors.grey.shade100,
+                                filled: true,
+
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              TextButton(
+                                onPressed: () {
+                                  setState(() => premium= "10");
+                                  // Navigator.pushNamed(context, 'register');
+                                },
+                                child: Text(
+                                  'Predict Premium',
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                      decoration: TextDecoration.underline,
+                                      color: Color(0xff4c505b),
+                                      fontSize: 18),
+                                ),
+                                style: ButtonStyle(),
+                              ),
+                              TextButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                    'Apply',
+                                    style: TextStyle(
+                                      decoration: TextDecoration.underline,
+                                      color: Color(0xff4c505b),
+                                      fontSize: 18,
+                                    ),
+                                  )),
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
